79f0545dd8b01922427b6cda3839a330d7bf4dd5
Kavleer <chriskabler@yahoo.com>
2019-10-22 14:40:03 -0400

    markdownify working

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from notes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.notes_list, name='notes_list'), path('notes/<id>/', views.notes_detail, name="notes_detail"),
 ]
